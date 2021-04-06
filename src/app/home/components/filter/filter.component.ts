5c16e0169e54b1c36f9bfab34695ef8282d2bac0
nadia <nadia.margescu@bearingpoint.com>
2021-04-06 23:56:20 +0300

    added dinamic filters

--- src/app/home/components/filter/filter.component.ts
+++ src/app/home/components/filter/filter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { IngredientsItem } from '../../services/dataModel/ingredientItem';
 import { IngredientsService } from '../../services/IngredientsService';
 
@@ -12,30 +12,46 @@ export class FilterComponent implements OnInit {
 
   public ingredients: IngredientsItem[];
 
-  public formSelect = new FormControl('');
+  // public formSelect = new FormControl('');
+
+  public myFormGroup: FormGroup;
 
 
   constructor(private ingredientsService: IngredientsService) { }
 
   public ngOnInit() {
-    this.getValues();
+    // this.getValues();
     this.ingredientsService.getAllIngredients().subscribe(ingredients => {
       console.log(ingredients);
       this.ingredients = ingredients;
+      let group={}
+      this.ingredients.forEach(ingredient =>{
+        group[ingredient.name] = new FormControl('');
+      })
+      this.myFormGroup = new FormGroup(group);
     });
+
+    // let group={}
+    // this.ingredients.forEach(ingredient =>{
+    //   group[ingredient.name] = new FormControl('');
+    // })
+    // this.myFormGroup = new FormGroup(group);
+
   }
 
-
-
-  onSubmit(){
-    console.log(this.formSelect.value.ingredient1);
-    this.formSelect.patchValue({ingredient1: false});
-    // console.log(JSON.stringify(this.formSelect.getRawValue()));
+  public getIngredients() {
+    console.log(this.myFormGroup.value);
   }
 
-  public getValues(): void {
-    this.formSelect.valueChanges.subscribe((value) => {console.log(value)});
-  }
+  // onSubmit(){
+  //   console.log(this.formSelect.value.ingredient1);
+  //   this.formSelect.patchValue({ingredient1: false});
+  //   // console.log(JSON.stringify(this.formSelect.getRawValue()));
+  // }
+
+  // public getValues(): void {
+  //   this.formSelect.valueChanges.subscribe((value) => {console.log(value)});
+  // }
 
 }
 
