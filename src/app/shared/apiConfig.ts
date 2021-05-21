f0f4afa9370b02348571f3a34601569243783198
nadiamaria <nadiamaria353@gmail.com>
2021-05-22 00:12:31 +0300

    retete

--- src/app/shared/apiConfig.ts
+++ src/app/shared/apiConfig.ts
@@ -1,3 +1,9 @@
 export const ApiConfig = {
-  url : 'http://localhost:3000'
+  url : 'http://25.97.175.158:3000'
 };
+
+//url : 'http://25.97.175.158:3000'
+
+//url : 'http://localhost:3000' 
+
+//ng serve --host 25.97.175.158 --port 4201
